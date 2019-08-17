473705ee188f373eef786888f8b28816459553e7
kikiope Oyeniran <kikioyeniran@gmail.com>
2019-08-17 17:23:14 +0100

    ninth commit

--- models/cleaner.js
+++ models/cleaner.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
+const validator = require('validator');
 
 
 //Cleaner schema
@@ -15,6 +18,43 @@ const CleanerSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please confirm your password'],
+        validate: {
+          // This only works on CREATE and SAVE!!!
+          validator: function(el) {
+            return el === this.password;
+          },
+          message: 'Passwords are not the same!'
+        }
+      },
+    passwordResetToken: String,
+    passwordResetExpires: Date
 
 });
+
+CleanerSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+    console.log({resetToken}, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10*60*1000; //10mins
+    return resetToken;
+}
+CleanerSchema.methods.correctPassword =  function(
+    candidatePassword,
+    userPassword
+  ) {
+    return bcrypt.compare(candidatePassword, userPassword);
+  };
 const Cleaner = module.exports = mongoose.model('cleaner', CleanerSchema);
+
+module.exports.createUser = (newUser, callback)=> {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) =>{
+            newUser.password = hash;
+            newUser.save(callback);
+        })
+    })
+}
