d82af58eb3b0201659583df327057cfc9e9521f3
kikiope Oyeniran <kikioyeniran@gmail.com>
2019-08-17 19:08:44 +0100

    tenth commit

--- config/database.js
+++ config/database.js
@@ -1,4 +1,15 @@
-module.exports = {
-    database:'mongodb://localhost:27017/kvetdb',
-    secret: 'yoursecret'
-}
+// module.exports = {
+//     database:'mongodb://localhost:27017/kvetdb',
+//     secret: 'yoursecret'
+// }
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {
+        database:'mongodb+srv://kikioyeniran:cAncel78$@cluster0-9rntd.mongodb.net/test',
+        secret: 'yoursecret'
+    }
+  } else {
+    module.exports = {
+        database:'mongodb://localhost:27017/kvetdb',
+        secret: 'yoursecret'
+    }
+  }
