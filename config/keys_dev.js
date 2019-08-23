204cf156fff9439181277420bd3a6fd1ad6dc955
kikiope Oyeniran <kikioyeniran@gmail.com>
2019-08-23 03:07:54 +0100

    final deployment

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,8 @@
 module.exports = {
-    stripePublishableKey: 'pk_test_NKxDE1s7myX98D4LcMiFTHCU',
-    stripeSecretKey: 'sk_test_tbuLsxPTon20HrTew4RBKKLG'
+    stripePublishableKey: 'pk_test_EZaBrvU3yv2xbZpo86lS59ef00HhdfCWhU',
+    stripeSecretKey: 'sk_test_L2z8Xz2k1DXe8pTxYFHpPyTK00yBHxrTUJ'
+    //from kvet: pk_test_EZaBrvU3yv2xbZpo86lS59ef00HhdfCWhU
+    //from promise: pk_test_NKxDE1s7myX98D4LcMiFTHCU
+    //from kvet: sk_test_L2z8Xz2k1DXe8pTxYFHpPyTK00yBHxrTUJ
+    //from promise: sk_test_tbuLsxPTon20HrTew4RBKKLG
 }
