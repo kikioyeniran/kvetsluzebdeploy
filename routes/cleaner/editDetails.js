4166444f882b37d8c0f45ef7a27f3935a6a458a9
kikiope Oyeniran <kikioyeniran@gmail.com>
2019-10-31 01:46:55 +0100

    fix pic upload error

--- routes/cleaner/editDetails.js
+++ routes/cleaner/editDetails.js
@@ -2,90 +2,113 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
+const empty = require('is-empty');
 
 //Bring in cleaner models
-let Cleaner =  require('../../models/cleaner');
-let CleanerDetails =  require('../../models/cleanerDetails');
+let Cleaner = require('../../models/cleaner');
+let CleanerDetails = require('../../models/cleanerDetails');
 
 //Edit cleaner Details Process
-router.post('/:cleanerID/:id/', (req, res) =>{
-    console.log('code is here');
-    let cleaner = {};
-    cleaner.fullName = req.body.fullName;
-    //console.log(req.body.fullName);
-    cleaner.postcode = req.body.postcode;
-    cleaner.city = req.body.city;
-    cleaner.country = req.body.country;
-    cleaner.address = req.body.address;
-    cleaner.mobileNumber = req.body.mobileNumber;
-    cleaner.extraTasks = req.body.extraTasks;
-    cleaner.profile = req.body.profile;
-    cleaner.income = req.body.income;
-    let query = {cleanerID : req.params.cleanerID}
-    //console.log(query);
-    //console.log(req.params.cleanerID)
+router.post('/:cleanerID/:id/', (req, res) => {
+  console.log('code is here');
+  let cleaner = {};
+  cleaner.fullName = req.body.fullName;
+  //console.log(req.body.fullName);
+  cleaner.postcode = req.body.postcode;
+  cleaner.city = req.body.city;
+  cleaner.country = req.body.country;
+  cleaner.address = req.body.address;
+  cleaner.mobileNumber = req.body.mobileNumber;
+  cleaner.extraTasks = req.body.extraTasks;
+  cleaner.profile = req.body.profile;
+  cleaner.income = req.body.income;
+  let query = { cleanerID: req.params.cleanerID };
+  //console.log(query);
+  //console.log(req.params.cleanerID)
 
-    CleanerDetails.updateOne(query, cleaner, (err) =>{
-        if(err){
-            console.log(err);
-            return;
-        }else {
-            console.log('found and updated');
-            req.flash('success', 'Account Updated');
-            res.redirect('/cleaner/dashboard/home/'+req.params.id);
-        }
-    });
+  CleanerDetails.updateOne(query, cleaner, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log('found and updated');
+      req.flash('success', 'Account Updated');
+      res.redirect('/cleaner/dashboard/home/' + req.params.id);
+    }
+  });
 });
 
 //Edit Profile Picture
-router.post('/images/:cleanerID/:id', (req, res)=>{
-    console.log('form submitted');
-    const storage = multer.diskStorage({
-        destination: './public/uploads/',
-        filename: function(req, file, cb){
-          cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+router.post('/images/:cleanerID/:id', (req, res) => {
+  console.log('form submitted');
+  const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb) {
+      cb(
+        null,
+        file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+      );
+    }
+  });
+  function checkFileType(files, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(
+      path.extname(files.originalname).toLowerCase()
+    );
+    // Check mime
+    const mimetype = filetypes.test(files.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+  // Initialise Upload
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10000000 },
+    fileFilter: function(req, file, cb) {
+      checkFileType(file, cb);
+    }
+  }).single('profilePic');
+  upload(req, res, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      let cleaner = {};
+      if (empty(req.file)) {
+        console.log('no picture chosen');
+        Cleaner.findById(req.params.id, (err, cleaner) => {
+          //console.log(cleaner)
+          var query = { cleanerID: cleaner.cleanerID };
+          CleanerDetails.find(query, (err, cleaner_details) => {
+            //console.log(cleaner_details[0].fullName);
+            res.render('cleaner/cleaner_dashboard', {
+              cleaner: cleaner,
+              cleanerDetails: cleaner_details[0],
+              picUpdate: false
+            });
+          });
+        });
+        // res.redirect('/cleaner/dashboard/home/' + req.params.id);
+        return;
+      }
+      cleaner.profilePic = req.file.filename;
+      let query = { cleanerID: req.params.cleanerID };
+      CleanerDetails.updateOne(query, cleaner, err => {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          console.log('found and updated');
+          req.flash('success', 'Picture Updated');
+          res.redirect('/cleaner/dashboard/home/' + req.params.id);
         }
       });
-      function checkFileType(files, cb){
-        // Allowed ext
-        const filetypes = /jpeg|jpg|png|gif/;
-        // Check ext
-        const extname = filetypes.test(path.extname(files.originalname).toLowerCase());
-        // Check mime
-        const mimetype = filetypes.test(files.mimetype);
-
-        if(mimetype && extname){
-          return cb(null,true);
-        } else {
-          cb('Error: Images Only!');
-        }
-      }
-      // Initialise Upload
-    const upload = multer({
-        storage: storage,
-        limits:{fileSize: 10000000},
-        fileFilter: function(req, file, cb){
-            checkFileType(file, cb);
-        }
-        }).single('profilePic')
-        upload(req, res, (err) => {
-            if(err){
-                console.log(err);
-            }else{
-                let cleaner = {};
-                cleaner.profilePic = req.file.filename;
-                let query = {cleanerID : req.params.cleanerID};
-                CleanerDetails.updateOne(query, cleaner, (err) =>{
-                    if(err){
-                        console.log(err);
-                        return;
-                    }else {
-                        console.log('found and updated');
-                        req.flash('success', 'Picture Updated');
-                        res.redirect('/cleaner/dashboard/home/'+req.params.id);
-                    }
-                });
-            }
-        });
+    }
+  });
 });
 module.exports = router;
--- routes/client/editDetails.js
+++ routes/client/editDetails.js
@@ -7,83 +7,105 @@ const multer = require('multer');
 const path = require('path');
 
 //Bring in Client Models
-let Client =  require('../../models/client');
-let ClientDetails =  require('../../models/clientDetails');
+let Client = require('../../models/client');
+let ClientDetails = require('../../models/clientDetails');
 
 //Edit Client Details Process
-router.post('/:clientID/:id', (req, res) =>{
-    let client = {};
-    client.fullName = req.body.fullName;
-    console.log(req.body.fullName);
-    client.postcode = req.body.postcode;
-    client.city = req.body.city;
-    client.country = req.body.country;
-    client.address = req.body.address;
-    client.mobileNumber = req.body.mobileNumber;
-    let query = {clientID : req.params.clientID}
-    console.log(query);
-    console.log(req.params.clientID)
+router.post('/:clientID/:id', (req, res) => {
+  let client = {};
+  client.fullName = req.body.fullName;
+  console.log(req.body.fullName);
+  client.postcode = req.body.postcode;
+  client.city = req.body.city;
+  client.country = req.body.country;
+  client.address = req.body.address;
+  client.mobileNumber = req.body.mobileNumber;
+  let query = { clientID: req.params.clientID };
+  console.log(query);
+  console.log(req.params.clientID);
 
-    ClientDetails.updateOne(query, client, (err) =>{
-        if(err){
-            console.log(err);
-            return;
-        }else {
-            console.log('found and updated');
-            req.flash('success', 'Account Updated');
-            res.redirect('/client/dashboard/home/'+req.params.id);
-        }
-    });
+  ClientDetails.updateOne(query, client, err => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      console.log('found and updated');
+      req.flash('success', 'Account Updated');
+      res.redirect('/client/dashboard/home/' + req.params.id);
+    }
+  });
 });
 
-router.post('/images/:clientID/:id', (req, res)=>{
-    console.log('form submitted');
-    const storage = multer.diskStorage({
-        destination: './public/uploads/',
-        filename: function(req, file, cb){
-          cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+router.post('/images/:clientID/:id', (req, res) => {
+  console.log('form submitted');
+  const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb) {
+      cb(
+        null,
+        file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+      );
+    }
+  });
+  function checkFileType(files, cb) {
+    // Allowed ext
+    const filetypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = filetypes.test(
+      path.extname(files.originalname).toLowerCase()
+    );
+    // Check mime
+    const mimetype = filetypes.test(files.mimetype);
+
+    if (mimetype && extname) {
+      return cb(null, true);
+    } else {
+      cb('Error: Images only!');
+    }
+  }
+  // Initialise Upload
+  const upload = multer({
+    storage: storage,
+    limits: { fileSize: 10000000 },
+    fileFilter: function(req, file, cb) {
+      checkFileType(file, cb);
+    }
+  }).single('profilePic');
+  upload(req, res, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      let client = {};
+      if (empty(req.file)) {
+        console.log('no picture chosen');
+        Client.findById(req.params.id, (err, client) => {
+          //console.log(client)
+          var query = { clientID: client.clientID };
+          ClientDetails.find(query, (err, client_details) => {
+            //console.log(client_details[0]);
+            res.render('client/client_dashboard', {
+              client: client,
+              clientDetails: client_details[0],
+              picUpdate: false
+            });
+          });
+        });
+        // res.redirect('/cleaner/dashboard/home/' + req.params.id);
+        return;
+      }
+      client.profilePic = req.file.filename;
+      let query = { clientID: req.params.clientID };
+      ClientDetails.updateOne(query, client, err => {
+        if (err) {
+          console.log(err);
+          return;
+        } else {
+          console.log('found and updated');
+          req.flash('success', 'Picture Updated');
+          res.redirect('/client/dashboard/home/' + req.params.id);
         }
       });
-      function checkFileType(files, cb){
-        // Allowed ext
-        const filetypes = /jpeg|jpg|png|gif/;
-        // Check ext
-        const extname = filetypes.test(path.extname(files.originalname).toLowerCase());
-        // Check mime
-        const mimetype = filetypes.test(files.mimetype);
-
-        if(mimetype && extname){
-          return cb(null,true);
-        } else {
-          cb('Error: Images only!');
-        }
-      }
-      // Initialise Upload
-    const upload = multer({
-        storage: storage,
-        limits:{fileSize: 10000000},
-        fileFilter: function(req, file, cb){
-            checkFileType(file, cb);
-        }
-        }).single('profilePic')
-        upload(req, res, (err) => {
-            if(err){
-                console.log(err);
-            }else{
-                let client = {};
-                client.profilePic = req.file.filename;
-                let query = {clientID : req.params.clientID};
-                ClientDetails.updateOne(query, client, (err) =>{
-                    if(err){
-                        console.log(err);
-                        return;
-                    }else {
-                        console.log('found and updated');
-                        req.flash('success', 'Picture Updated');
-                        res.redirect('/client/dashboard/home/'+req.params.id);
-                    }
-                });
-            }
-        });
+    }
+  });
 });
 module.exports = router;
